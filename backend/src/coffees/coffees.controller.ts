073bfbe4d5af141b9a7d75f4b266efa126e8577f
Marcin Krawczyk <marckraw@icloud.com>
2021-05-02 21:12:54 +0200

    [add] nestjs with some sample controllers from tutorial

---
+++ backend/src/coffees/coffees.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get('')
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+  }
+
+  @Get('flavors')
+  findAllFlavors(@Res() response) {
+    response.status(200).send('This action returns all Flavors'); // using native Express methods
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
+  }
+
+  @Post()
+  // @HttpCode(HttpStatus.GONE) // you can add custom http status
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    console.log('id: ', id);
+    console.log('body: ', updateCoffeeDto);
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
