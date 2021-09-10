d61afceca323eaeb7272f8bcfb4463a7790d244d
Marcin Krawczyk <marckraw@icloud.com>
2021-09-10 15:25:25 +0200

    [frontend]: add useBoolean hook

---
+++ frontend/hooks/useBoolean.tsx
@@ -0,0 +1,19 @@
+import { useMemo } from 'react'
+import useToggle from './useToggle'
+
+function useBoolean(initialValue = false) {
+    const [value, toggle] = useToggle(initialValue)
+
+    const handlers = useMemo(
+        () => ({
+            toggle,
+            on: () => toggle(true),
+            off: () => toggle(false)
+        }),
+        [toggle]
+    )
+
+    return [value, handlers]
+}
+
+export default useBoolean
